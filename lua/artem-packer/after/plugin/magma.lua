035eb4fd84ff4a1cb311709a5e95da7e9f56fbb1
Artem3124 <mr.zhukovsky02@gmail.com>
2024-04-09 12:18:09 +0200

    magma updated

--- lua/artem-packer/after/plugin/magma.lua
+++ lua/artem-packer/after/plugin/magma.lua
@@ -1,13 +1,12 @@
 -- Key mappings
-vim.keymap.set('n', '<leader>e', ':MagmaEvaluateOperator<CR>', { silent = true, expr = true })
-vim.keymap.set('n', '<leader>ee', ':MagmaEvaluateLine<CR>', { silent = true })
-vim.keymap.set('x', '<leader>e', ':<C-u>MagmaEvaluateVisual<CR>', { silent = true })
-vim.keymap.set('n', '<leader>ec', ':MagmaReevaluateCell<CR>', { silent = true })
-vim.keymap.set('n', '<leader>ed', ':MagmaDelete<CR>', { silent = true })
-vim.keymap.set('n', '<leader>eo', ':MagmaShowOutput<CR>', { silent = true })
+vim.keymap.set('n', '<leader>r', ':MagmaEvaluateOperator<CR>', { silent = true, expr = true })
+vim.keymap.set('n', '<leader>rr', ':MagmaEvaluateLine<CR>', { silent = true })
+vim.keymap.set('x', '<leader>r', ':<C-u>MagmaEvaluateVisual<CR>', { silent = true })
+vim.keymap.set('n', '<leader>rc', ':MagmaReevaluateCell<CR>', { silent = true })
+vim.keymap.set('n', '<leader>rd', ':MagmaDelete<CR>', { silent = true })
+vim.keymap.set('n', '<leader>ro', ':MagmaShowOutput<CR>', { silent = true })
 vim.keymap.set('n', '<leader>mi', ':MagmaInit<CR>', { silent = true })
 
 -- Settings
 vim.g.magma_automatically_open_output = true
-vim.g.magma_image_provider = 'kitty'
 
