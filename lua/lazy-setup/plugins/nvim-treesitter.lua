02a739898d202e602456da0268d253095ea31802
Artem3124 <mr.zhukovsky02@gmail.com>
2024-05-17 08:42:33 +0200

    treesitter fixes

--- lua/lazy-setup/plugins/nvim-treesitter.lua
+++ lua/lazy-setup/plugins/nvim-treesitter.lua
@@ -9,7 +9,7 @@ return {
             disable = { "txt" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+            sync_install = true,
 
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
