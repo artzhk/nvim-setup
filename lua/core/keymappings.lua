61ce4cb6bee3b3ca9f95eae353e1a657381399f4
Artem3124 <mr.zhukovsky02@gmail.com>
2024-08-12 21:03:56 +0200

    dvorak adjustments

--- lua/core/keymappings.lua
+++ lua/core/keymappings.lua
@@ -62,7 +62,6 @@ vim.keymap.set("n", "<leader>K", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>J", "<cmd>lprev<CR>zz")
 
 -- Inside of file renaming and making executable
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- quick navigation to remaps
--- lua/lazy-setup/plugins/telescope.lua
+++ lua/lazy-setup/plugins/telescope.lua
@@ -1,86 +1,91 @@
 return {
-    { "nvim-telescope/telescope-ui-select.nvim", },
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local builtin = require('telescope.builtin')
-            local utils = require('telescope.utils')
-            local actions = require('telescope.actions')
-            local action_layout = require('telescope.actions.layout')
+	{ "nvim-telescope/telescope-ui-select.nvim" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			local utils = require("telescope.utils")
+			local actions = require("telescope.actions")
+			local action_layout = require("telescope.actions.layout")
 
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-                defaults = {
-                    layout_strategy = 'vertical',
-                    layout_config = {
-                        vertical = {
-                            preview_height = 0.7,
-                            width = 0.9,
-                            horizontal = 0.9,
-                            prompt_position = "top",
-                        },
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+				defaults = {
+					layout_strategy = "vertical",
+					layout_config = {
+						vertical = {
+							preview_height = 0.7,
+							width = 0.9,
+							horizontal = 0.9,
+							prompt_position = "top",
+						},
+					},
+					sorting_strategy = "ascending",
+					color_devicons = true,
+					file_ignore_patterns = {
+						"node_modules",
+						".git$", --[[ "lib/*" ]] --[[ "assets/*" ]]
+					},
+					mappings = {
+						i = {
+							["<C-c>"] = { "<esc>", type = "command" },
+							["<esc>"] = actions.close,
+							["<C-o>"] = action_layout.toggle_preview,
+							["<C-r>"] = actions.delete_buffer,
+						},
+						n = {
+							["<C-c>"] = actions.close,
+							["<C-r>"] = actions.delete_buffer,
+							["<C-o>"] = action_layout.toggle_preview,
+						},
+					},
+				},
+			})
 
-                    },
-                    sorting_strategy = "ascending",
-                    color_devicons = true,
-                    file_ignore_patterns = { "node_modules", ".git$", --[[ "lib/*" ]] --[[ "assets/*" ]] },
-                    mappings = {
-                        i = {
-                            ["<C-c>"] = { "<esc>", type = "command" },
-                            ["<esc>"] = actions.close,
-                            ["<C-o>"] = action_layout.toggle_preview,
-                            ["<C-r>"] = actions.delete_buffer,
-                        },
-                        n = {
-                            ["<C-c>"] = actions.close,
-                            ["<C-r>"] = actions.delete_buffer,
-                            ["<C-o>"] = action_layout.toggle_preview,
-                        },
-                    },
-                },
-            })
+			-- View current buffers
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 
-            -- View current buffers
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			-- -- Files search
+			-- vim.keymap.set('n', '<leader>fj', builtin.find_files, {})
 
-            -- Files search
-            vim.keymap.set('n', '<leader>fj', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+			-- Files search Dvorak
+			vim.keymap.set("n", "<leader>uh", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 
-            -- File search relative to current file's directory
-            vim.keymap.set('n', '<leader>df', function()
-                builtin.find_files({ cwd = utils.buffer_dir() })
-            end, {})
+			-- File search relative to current file's directory
+			vim.keymap.set("n", "<leader>df", function()
+				builtin.find_files({ cwd = utils.buffer_dir() })
+			end, {})
 
-            vim.keymap.set('n', '<leader>gs', function()
-                local word = vim.fn.expand("<cword>")
-                builtin.grep_string({ search = word })
-            end)
+			vim.keymap.set("n", "<leader>gs", function()
+				local word = vim.fn.expand("<cword>")
+				builtin.grep_string({ search = word })
+			end)
 
-            vim.keymap.set('n', '<leader>gS', function()
-                local word = vim.fn.expand("<cWORD>")
-                builtin.grep_string({ search = word })
-            end)
+			vim.keymap.set("n", "<leader>gS", function()
+				local word = vim.fn.expand("<cWORD>")
+				builtin.grep_string({ search = word })
+			end)
 
-            -- Grep relative to opened project directory
-            vim.keymap.set('n', '<leader>mg', function()
-                builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            end)
+			-- Grep relative to opened project directory
+			vim.keymap.set("n", "<leader>mg", function()
+				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			end)
 
-            -- Grep relative to current file's directory
-            vim.keymap.set('n', '<leader>rdf', function()
-                builtin.grep_string({ search = vim.fn.input("Relative Grep > "), cwd = utils.buffer_dir() })
-            end)
+			-- Grep relative to current file's directory
+			vim.keymap.set("n", "<leader>rdf", function()
+				builtin.grep_string({ search = vim.fn.input("Relative Grep > "), cwd = utils.buffer_dir() })
+			end)
 
-            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+			vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
 
-            require("telescope").load_extension("ui-select")
-        end,
-    },
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
