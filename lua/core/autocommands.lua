487d998e42fd8c64b4f170b151502178edbb4847
Artem3124 <mr.zhukovsky02@gmail.com>
2024-08-14 09:19:12 +0200

    gitsigns

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,15 +1,6 @@
 local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
 local gitlens = require("core.utils.gitlens")
 
-vim.api.nvim_create_autocmd({ "CursorMovedI" }, {
-    pattern = "*",
-    desc = "Clear blame virt text",
-    callback = function()
-        gitlens.clearBlameVirtText()
-    end,
-    group = autocmd_group,
-})
-
 vim.api.nvim_create_autocmd({ "CursorMoved" }, {
     pattern = "*",
     desc = "Clear blame virt text",
