8de16feea0a2819814372b3b167812b09d4783e4
Artem3124 <mr.zhukovsky02@gmail.com>
2024-08-21 23:18:23 +0200

    harpoon

--- lua/lazy-setup/plugins/harpoon.lua
+++ lua/lazy-setup/plugins/harpoon.lua
@@ -35,12 +35,6 @@ return {
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 
-        -- -- Dvorak
-        -- vim.keymap.set("n", "<C-a>", function() harpoon:list():select(1) end)
-        -- vim.keymap.set("n", "<C-p>", function() harpoon:list():select(2) end)
-        -- vim.keymap.set("n", "<C-y>", function() harpoon:list():select(3) end)
-        -- vim.keymap.set("n", "<C-f>", function() harpoon:list():select(4) end)
-
         vim.keymap.set("n", "<C-a>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-f>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-y>", function() harpoon:list():select(3) end)
