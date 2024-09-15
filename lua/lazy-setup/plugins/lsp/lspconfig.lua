135237132583aeefd4aabbd62d62b11a3d1f4f2d
Artem3124 <mr.zhukovsky02@gmail.com>
2024-09-15 17:51:32 +0200

    dev sync

--- lua/lazy-setup/plugins/lsp/lspconfig.lua
+++ lua/lazy-setup/plugins/lsp/lspconfig.lua
@@ -1,3 +1,86 @@
+local M = {
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+}
+
+function M.init(lsp_zero, cmp_lsp, lsp_config)
+	M.lsp_zero = lsp_zero
+	M.cmp_lsp = cmp_lsp
+	M.lsp_config = lsp_config
+end
+
+function M.capabilities()
+	return vim.tbl_deep_extend(
+		"force",
+		{},
+		vim.lsp.protocol.make_client_capabilities(),
+		M.cmp_lsp.default_capabilities()
+	)
+end
+
+function M.config_keymaps(client, bufnr)
+	M.lsp_zero.default_keymaps({ buffer = bufnr })
+
+	local opts = { buffer = bufnr, remap = true }
+
+	if client.name == "omnisharp" or client.name == "cs" then
+		vim.keymap.set("n", "gd", "<cmd>lua require('omnisharp_extended').lsp_definitions()<cr>", opts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		vim.keymap.set("n", "<leader>vws", function()
+			vim.lsp.buf.workspace_symbol(vim.fn.input("Grep > "))
+		end, opts)
+		vim.keymap.set("n", "<leader>q", vim.diagnostic.open_float)
+		vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
+		vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
+		vim.keymap.set("n", "<leader>va", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrr", "<cmd>lua require('omnisharp_extended').lsp_references()<cr>", opts)
+		vim.keymap.set("n", "<leader>rn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "<leader>vi", "<cmd>lua require('omnisharp_extended').lsp_implementation()<cr>", opts)
+		vim.keymap.set("n", "<leader>vt", "<cmd>lua require('omnisharp_extended').lsp_type_definition()<cr>", opts)
+
+		return
+	end
+
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol(vim.fn.input("Grep > "))
+	end, opts)
+	vim.keymap.set("n", "<leader>q", vim.diagnostic.open_float)
+	vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
+	vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
+	vim.keymap.set("n", "<leader>va", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>rn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+end
+
 return {
 	"VonHeikemen/lsp-zero.nvim",
 	dependencies = {
@@ -24,150 +107,58 @@ return {
 	},
 	config = function()
 		local lsp_zero = require("lsp-zero")
-		lsp_zero.extend_lspconfig()
-		-- require("omnisharp_extended").lsp_definitions()
-		-- local ts_lsp = require('lazy-setup.plugins.lsp.ts-lsp')
-
-		lsp_zero.on_attach(function(client, bufnr)
-			lsp_zero.default_keymaps({ buffer = bufnr })
-			-- disabling for cs
-			-- print(client.name)
-
-			if client.name == "omnisharp" or client.name == "cs" then
-				local opts = { buffer = bufnr, remap = true }
-
-				vim.keymap.set("n", "gd", "<cmd>lua require('omnisharp_extended').lsp_definitions()<cr>", opts)
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, opts)
-				vim.keymap.set("n", "<leader>vws", function()
-					vim.lsp.buf.workspace_symbol(vim.fn.input("Grep > "))
-				end, opts)
-				vim.keymap.set("n", "<leader>q", vim.diagnostic.open_float)
-				vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
-				vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
-				vim.keymap.set("n", "<leader>va", function()
-					vim.lsp.buf.code_action()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrr", "<cmd>lua require('omnisharp_extended').lsp_references()<cr>", opts)
-				vim.keymap.set("n", "<leader>rn", function()
-					vim.lsp.buf.rename()
-				end, opts)
-				vim.keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, opts)
-				vim.keymap.set(
-					"n",
-					"<leader>vi",
-					"<cmd>lua require('omnisharp_extended').lsp_implementation()<cr>",
-					opts
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>vt",
-					"<cmd>lua require('omnisharp_extended').lsp_type_definition()<cr>",
-					opts
-				)
-
-				return
-			end
-
-			-- see :help lsp-zero-keybindings
-			-- to learn the available actions
-			lsp_zero.default_keymaps({ buffer = bufnr })
-
-			local opts = { buffer = bufnr, remap = false }
-
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, opts)
-			vim.keymap.set("n", "K", function()
-				vim.lsp.buf.hover()
-			end, opts)
-			vim.keymap.set("n", "<leader>vws", function()
-				vim.lsp.buf.workspace_symbol(vim.fn.input("Grep > "))
-			end, opts)
-			vim.keymap.set("n", "<leader>q", vim.diagnostic.open_float)
-			vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "<leader>va", function()
-				vim.lsp.buf.code_action()
-			end, opts)
-			vim.keymap.set("n", "<leader>vrr", function()
-				vim.lsp.buf.references()
-			end, opts)
-			vim.keymap.set("n", "<leader>rn", function()
-				vim.lsp.buf.rename()
-			end, opts)
-			vim.keymap.set("i", "<C-h>", function()
-				vim.lsp.buf.signature_help()
-			end, opts)
-		end)
-
-		local autocommands = require("core.autocommands")
-		local lsp_config = require("lspconfig")
-		--local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
+		local lsp_config = require("lspconfig")
 
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities()
-		)
+		M.init(lsp_zero, cmp_lsp, lsp_config)
+		M.lsp_zero.extend_lspconfig()
+		M.lsp_zero.on_attach(M.config_keymaps)
+
+		local autocommands = require("core.autocommands")
 
 		local pyright_config = require("lazy-setup.configs.ls.pyright")
 		local tsserver_config = require("lazy-setup.configs.ls.tsserver")
 		local volar_config = require("lazy-setup.configs.ls.volar")
 
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+		require("mason").setup(M.ui)
+
 		require("mason-lspconfig").setup({
-			-- for web
-			-- ensure_installed = { "lua_ls", "angularls", "pyright", "html", "clangd", omnisharp, "tsserver", "eslint"},
 			ensure_installed = { "lua_ls", "tsserver", "clangd" },
 			handlers = {
-				lsp_zero.default_setup,
+				M.lsp_zero.default_setup,
 				function(server)
 					lsp_config[server].setup({
-						capabilities = capabilities,
-						on_attach = lsp_zero.on_attach,
+						capabilities = M.capabilities(),
+						on_attach = M.lsp_zero.on_attach,
 					})
 				end,
 
 				["omnisharp"] = function()
 					require("omnisharp_extended").lsp_definitions()
 					lsp_config.omnisharp.setup({
-						capabilities = capabilities,
-						on_attach = lsp_zero.on_attach,
+						capabilities = M.capabilities(),
+						on_attach = M.lsp_zero.on_attach,
 					})
 				end,
 				["eslint"] = function()
 					lsp_config.eslint.setup({
-						capabilities = capabilities,
+						capabilities = M.capabilities(),
 						on_attach = autocommands.eslint_on_save,
 					})
 				end,
 				["pyright"] = function()
 					lsp_config.pyright.setup({
-						capabilities = capabilities,
-						on_attach = lsp_zero.on_attach,
+						capabilities = M.capabilities(),
+						on_attach = M.lsp_zero.on_attach,
 						default_config = pyright_config.default_config,
 						commands = pyright_config.commands,
 					})
 				end,
 				["tsserver"] = function()
 					lsp_config.tsserver.setup({
-						capabilities = capabilities,
-						on_attach = lsp_zero.on_attach,
+						capabilities = M.capabilities(),
+						on_attach = M.lsp_zero.on_attach,
 						init_options = tsserver_config.init_options(false),
 						filetypes = tsserver_config.filetypes,
 					})
@@ -177,30 +168,30 @@ return {
 					lsp_config.volar.setup({
 						cmd = volar_config.cmd,
 						filetypes = volar_config.filetypes,
-						capabilities = capabilities,
-						on_attach = lsp_zero.on_attach,
+						capabilities = M.capabilities(),
+						on_attach = M.lsp_zero.on_attach,
 						init_options = volar_config.init_options,
 					})
 				end,
 				["clangd"] = function()
 					lsp_config.clangd.setup({
-						capabilities = capabilities,
-						on_attach = lsp_zero.on_attach,
+						capabilities = M.capabilities(),
+						on_attach = M.lsp_zero.on_attach,
 						root_dir = lsp_config.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
 					})
 				end,
 				["angularls"] = function()
 					lsp_config.angularls.setup({
-						capabilities = capabilities,
-						on_attach = lsp_zero.on_attach,
+						capabilities = M.capabilities(),
+						on_attach = M.lsp_zero.on_attach,
 						root_dir = lsp_config.util.root_pattern("angular.json", "package.json"),
 					})
 				end,
 				["lua_ls"] = function()
-					local lua_opts = lsp_zero.nvim_lua_ls()
+					local lua_opts = M.lsp_zero.nvim_lua_ls()
 					lsp_config.lua_ls.setup({
-						capabilities = capabilities,
-						on_attach = lsp_zero.on_attach,
+						capabilities = M.capabilities(),
+						on_attach = M.lsp_zero.on_attach,
 						settings = {
 							Lua = {
 								diagnostics = {
@@ -214,8 +205,8 @@ return {
 			},
 		})
 
-		lsp_zero.setup()
-		local cmp_action = lsp_zero.cmp_action()
+		M.lsp_zero.setup()
+		local cmp_action = M.lsp_zero.cmp_action()
 		local ls = require("luasnip")
 		require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -231,31 +222,6 @@ return {
 				{ name = "buffer" },
 				{ name = "nvim_lua" },
 			},
-			-- mapping = {
-			--     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-			--     ['<C-<Tab>>'] = cmp.mapping(function ()
-			--         if ls.expandable() then
-			--             ls.expand()
-			--         elseif cmp.visible() then
-			--             cmp.select_next_item()
-			--         elseif ls.jumpable(1) then
-			--             ls.jump(1)
-			--         else
-			--             fallback()
-			--         end
-			--     end, { 'i', 's' }),
-			--     ['<Tab>'] = cmp.mapping(function(fallback)
-			--     end, { 'i', 's' }),
-			--     ['<S-Tab>'] = cmp.mapping(function(fallback)
-			--         if ls.jumpable(-1) then
-			--             ls.jump(-1)
-			--         elseif cmp.visible() then
-			--             cmp.select_prev_item()
-			--         else
-			--             fallback()
-			--         end
-			--     end, { 'i', 's' })
-			-- },
 			mapping = cmp.mapping.preset.insert({
 				["<Tab>"] = cmp_action.luasnip_supertab(),
 				["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
